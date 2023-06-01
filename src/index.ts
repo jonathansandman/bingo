d1ff5b7ac07c14a67b089fdbb882793693239eed
Jon Sandman <jonathans@solidstatelogic.com>
2023-06-01 23:26:35 +0100

    add second squid task to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { BingoCard } from "./classes/BingoCard";
 import { splitStringByEmptyLine } from "./utils/splitStringByEmptyLine";
 
-const input = `
+const input1 = `
 7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
 
 22 13 17 11 0
@@ -11,12 +11,40 @@ const input = `
 1 12 20 15 19
 `;
 
+const input2 = `
+7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+
+22 13 17 11 0
+8 2 23 4 24
+21 9 14 16 7
+6 10 3 18 5
+1 12 20 15 19
+
+3 15 0 2 22
+9 18 13 17 5
+19 8 7 25 23
+20 11 10 24 4
+14 21 16 12 6
+
+14 21 17 24 4
+10 16 15 9 19
+18 8 23 26 20
+22 11 13 6 5
+2 0 12 3 7
+`;
+
 export function main(input: string) {
-	const [calledStr, cardStr] = splitStringByEmptyLine(input);
+	const [calledStr, ...cardsStr] = splitStringByEmptyLine(input);
 	const numbersToCall = calledStr.split(",").map((x) => parseInt(x, 10));
-	const card = new BingoCard(cardStr);
-	const result = card.callNumbers(numbersToCall);
-	console.log(result);
+	const cards = cardsStr.map((x) => new BingoCard(x));
+	for (const card of cards) {
+		const result = card.callNumbers(numbersToCall);
+		console.log(`Bingo: ${result}`);
+	}
 }
 
-main(input);
+console.log("[First task]");
+main(input1);
+
+console.log("[Second task]");
+main(input2);
