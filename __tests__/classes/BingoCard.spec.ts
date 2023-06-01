516dc4f662cf63ca863ca7e8ff515ef695c8b326
Jon Sandman <jonathans@solidstatelogic.com>
2023-06-01 23:01:41 +0100

    add card width/height properties

--- __tests__/classes/BingoCard.spec.ts
+++ __tests__/classes/BingoCard.spec.ts
@@ -15,4 +15,32 @@ describe("BingoCard", () => {
 			[7, 8, 9],
 		]);
 	});
+
+	it("should correctly handle different input strings and create cards with the correct height and width", () => {
+		const input1 = `
+			1 2
+			3 4
+		`;
+		const card1 = new BingoCard(input1);
+		expect(card1.getCard()).toEqual([
+			[1, 2],
+			[3, 4],
+		]);
+		expect(card1.getWidth()).toBe(2);
+		expect(card1.getHeight()).toBe(2);
+
+		const input2 = `
+			1 2 3 4
+			5 6 7 8
+			9 10 11 12
+		`;
+		const card2 = new BingoCard(input2);
+		expect(card2.getCard()).toEqual([
+			[1, 2, 3, 4],
+			[5, 6, 7, 8],
+			[9, 10, 11, 12],
+		]);
+		expect(card2.getWidth()).toBe(4);
+		expect(card2.getHeight()).toBe(3);
+	});
 });
--- src/classes/BingoCard.ts
+++ src/classes/BingoCard.ts
@@ -1,6 +1,8 @@
 export class BingoCard {
 	private card: number[][];
-	
+	private width: number;
+	private height: number;
+
 	static parseStringToCard(str: string) {
 		return str
 			.trim()
@@ -15,9 +17,19 @@ export class BingoCard {
 
 	constructor(input: string) {
 		this.card = BingoCard.parseStringToCard(input);
+		this.width = this.card[0]?.length || 0;
+		this.height = this.card.length;
 	}
 
 	getCard(): number[][] {
 		return this.card;
+	}
+
+	getWidth(): number {
+		return this.width;
+	}
+
+	getHeight(): number {
+		return this.height;
 	}
 }
