49e5ebc93ec9779e2f535c9057f695067f650620
Jon Sandman <jonathans@solidstatelogic.com>
2023-06-02 10:15:49 +0100

    add some documentation

--- src/utils/splitStringByEmptyLine.ts
+++ src/utils/splitStringByEmptyLine.ts
@@ -1,3 +1,8 @@
+/**
+ * Splits a string into blocks based on empty lines. Discards any empty strings.
+ * @param {string} str - The input string to split.
+ * @returns {string[]} An array of blocks split from the input string.
+ */
 export function splitStringByEmptyLine(str: string) {
 	return str.split(/\n\s*\n/).filter((block) => block.trim().length > 0);
 }
